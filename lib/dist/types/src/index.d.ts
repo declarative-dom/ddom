518ee65d8fcc0c927e4dea821b232aa644ef4cca
Kevin Shenk <kevin@avu.nu>
2025-06-18 11:51:28 -0400

    chore: remove dist files

--- lib/dist/types/src/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { MappedArrayExpr, CustomElementSpec, DocumentSpec, DOMNode, DOMSpec, ElementSpec, FilterExpr, FilterOper, HTMLBodyElementSpec, HTMLElementSpec, HTMLHeadElementSpec, Operator, SortExpr, StyleExpr, WindowSpec, } from './types';
